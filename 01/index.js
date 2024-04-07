db66d4b0fad8e8ec1c08661e0d92a463754d8f34
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-04-07 23:17:27 +0200

    01.1

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Paragraph = () => {
+  return React.createElement(
+    'p',
+    {
+      style: {
+        backgroundColor: 'blue',
+        fontSize: 'bold',
+      },
+      id: 'first-paragraph',
+      className: 'paragraph',
+    },
+    'Lorem ipsum dolor sit amet',
+  );
+};
+
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(Paragraph(), appContainer);
