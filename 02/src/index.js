3c9af0d26744db2fce02d7673220f12f845a55e8
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-04-08 23:04:37 +0200

    02.2.1

--- 02/src/index.js
+++ 02/src/index.js
@@ -1,8 +1,8 @@
 import { data } from './data.js';
 import { combineAge, renderAge } from './helpers.js';
+import camelCase from 'lodash/camelCase';
 
+console.log(camelCase('hello roman co tam słychać'));
 console.log(combineAge(data));
-
 const container = document.querySelector('#age');
-
 renderAge(container, data);
