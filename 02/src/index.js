446e629ea5c688fa773e2b96a3650204a5e9ba86
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-04-08 22:06:47 +0200

    02.1

---
+++ 02/src/index.js
@@ -0,0 +1,8 @@
+import { data } from './data.js';
+import { combineAge, renderAge } from './helpers.js';
+
+console.log(combineAge(data));
+
+const container = document.querySelector('#age');
+
+renderAge(container, data);
